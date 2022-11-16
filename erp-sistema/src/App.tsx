daa153936dda5ddcb10996fa3aa7967f21e81381
Maik-Biazi <maikwilliam4553@gmail.com>
2022-11-15 22:07:44 -0300

    rotas-criadas

--- erp-sistema/src/App.tsx
+++ erp-sistema/src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
-  return <div className="App">
-      ola
-  </div>;
+  return (
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
+  );
 };
